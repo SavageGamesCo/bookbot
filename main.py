c879c207b3c840672b5781b2ec5929074aca9bd7
SavageGamesCo <brutallynx@gmail.com>
2024-03-11 10:30:38 -0400

    second save

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     filepath = "books/frankenstein.txt"
     document = read_document(filepath)
     print(f"{words(document)} words found in the document.")
-    print(f"There are {count_letters(document)} letters in the document.")
+    print("")
+    count_letters(document)
     
 
 def read_document(file):
@@ -16,13 +17,16 @@ def words(contents):
     return len(word_count)
 
 def count_letters(contents):
-    letter_count = [{"letter" : "num"}]
+    letter_count = {}
     lowercase_content = contents.lower()
     for letter in lowercase_content:
         if letter in letter_count:
-            letter_count ["letter":"num"] +=1
+            letter_count [letter] +=1
         else:
-            letter_count["letter":"num"] = 1
-    letter_count.sort(key=letter_count["num"])
-    return letter_count
+            letter_count[letter] = 1
+    list_of_letter_dicts = [letter for letter in letter_count.items()]
+    list_of_letter_dicts.sort(key=lambda item: item[1])
+    for item in list_of_letter_dicts:
+        print(f"The letter '{item[0]}' appears {item[1]} times in the document.")
+    
 main()
